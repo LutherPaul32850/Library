6f4a0f1447d8717acce4f8886f3a685d4772592a
LutherPaul32850 <2200032850@kluniversity.in>
2024-12-11 00:35:26 +0530

    hello

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -6,9 +6,9 @@ import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import { LatestReviews } from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
-import BookModel from "../../models/BookModel";
 import ReviewModel from "../../models/ReviewModel";
 import ReviewRequestModel from "../../models/ReviewRequestModel";
+import BookModel from "../../models/BookModel";
 
 
 export const BookCheckoutPage = () => {
